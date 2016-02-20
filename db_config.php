9771171c170a93167637780078aaf59ac69af033
santos22 <santosjavier22@gmail.com>
2016-02-19 22:27:56 -0600

    Added class files to hold connection variables and connect to our database

---
+++ db_config.php
@@ -0,0 +1,12 @@
+db_config.php
+<?php
+ 
+/*
+ * All database connection variables
+ */
+ 
+define('DB_USER', "root"); // db user
+define('DB_PASSWORD', "team6"); // db password
+define('DB_DATABASE', "Team6"); // database name
+define('DB_SERVER', "ec2-54-144-242-152.compute-1.amazonaws.com"); // db server
+?>
