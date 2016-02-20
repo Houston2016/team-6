27039b8de2dba6638a23c3984a37ffa5bbbf8776
santos22 <santosjavier22@gmail.com>
2016-02-19 22:38:27 -0600

    Created new directory and removed php file

--- create.php
+++
@@ -1,8 +0,0 @@
-<?php
-$con=mysqli_connect("ec2-54-144-242-152.compute-1.amazonaws.com","root","team6");
-$sql="CREATE DATABASE my_db";
-if (mysqli_query($con,$sql))
-{
-   echo "Database my_db created successfully";
-}
-?>
