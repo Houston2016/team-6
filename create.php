602d6bc88df1fffef9ae35632765c40466f3faf1
santos22 <santosjavier22@gmail.com>
2016-02-19 22:05:27 -0600

    Creating a test DB table using PHP

--- create.php
+++ create.php
@@ -1,22 +1,8 @@
 <?php
-   $dbhost = 'ec2-54-144-242-152.compute-1.amazonaws.com';
-   $dbuser = 'root';
-   $dbpass = 'team6';
-   $conn = mysql_connect($dbhost, $dbuser, $dbpass);
-   
-   if(! $conn ) {
-      die('Could not connect: ' . mysql_error());
-   }
-   
-   echo 'Connected successfully';
-   
-   $sql = 'CREATE Database test_db';
-   $retval = mysql_query( $sql, $conn );
-   
-   if(! $retval ) {
-      die('Could not create database: ' . mysql_error());
-   }
-   
-   echo "Database test_db created successfully\n";
-   mysql_close($conn);
+$con=mysqli_connect("ec2-54-144-242-152.compute-1.amazonaws.com","root","team6");
+$sql="CREATE DATABASE my_db";
+if (mysqli_query($con,$sql))
+{
+   echo "Database my_db created successfully";
+}
 ?>
